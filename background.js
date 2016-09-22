879901a84947907ea31175860d33e4bb802a0841
Luis Rita <luismestrerita@gmail.com>
2016-09-22 10:03:02 +0100

    tryed to implement example of DOM request extentention

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // React when a browser action's icon is clicked.
-chrome.browserAction.onClicked.addListener(function(tab) {
+/*chrome.browserAction.onClicked.addListener(function(tab) {
   var viewTabUrl = chrome.extension.getURL('image.html');
-  var imageUrl = /* an image's URL */;
+  var imageUrl = /* an image's URL ;
 
   // Look through all the pages in this extension to find one we can use.
   var views = chrome.extension.getViews();
@@ -17,4 +17,22 @@ chrome.browserAction.onClicked.addListener(function(tab) {
       break; // we're done
     }
   }
+});*/
+
+// Regex-pattern to check URLs against. 
+// It matches URLs like: http[s]://[...]stackoverflow.com[...]
+var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?stackoverflow\.com/;
+
+// A function to use as callback
+function doStuffWithDom(domContent) {
+    console.log('I received the following DOM content:\n' + domContent);
+}
+
+// When the browser-action button is clicked...
+chrome.browserAction.onClicked.addListener(function (tab) {
+    // ...check the URL of the active tab against our pattern and...
+    if (urlRegex.test(tab.url)) {
+        // ...if it matches, send a message specifying a callback too
+        chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
+    }
 });
