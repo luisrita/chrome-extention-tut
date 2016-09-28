0b37aacbfdda2a32bf48b94dbacd64b0b61dfcab
Luis Rita <luismestrerita@gmail.com>
2016-09-28 10:27:20 +0100

    update backup code

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ var odds = {},
 for (var i = 0; i <= lines.length; i++) {
   var thisLine = lines[i];
 
-  console.log(thisLine);
+  console.log(lines[i]);
   var event = $(thisLine).find('.event').html().split(',')[0],
       odd = $(thisLine).find('.odd p').html(),
       canceled = $(thisLine).find('.result').hasClass('nofinish'),
@@ -12,9 +12,8 @@ for (var i = 0; i <= lines.length; i++) {
       repeated = false;
 
   if(event !== 'Futebol') {
-    var oddsNumber = odds.length;
 
-    // Get unic values that are on odds
+    // Get unique values that are on the odds
     var objArray = Object.keys(odds);
     objArray.forEach(function (item, index) {
       if(odd === item) {
@@ -28,33 +27,38 @@ for (var i = 0; i <= lines.length; i++) {
       }
     });
 
-    // Push new unic value to odds
+    // Push new unique value to odds
     if(!repeated && !canceled) {
       // odds.push(odd);
       odds[odd] = {
-        wins: 0,
-        loses: 0,
+        wins: result === 'win' ? 1 : 0,
+        loses: result === 'lose' ? 1 : 0,
       };
-
-      if(result === 'win') {
-        odds[odd].wins += 1;
-      } else {
-        odds[odd].loses += 1;
-      }
     }
   }
 };
 
 function calcPercentages() {
   for(var o in odds) {
-  var wins = parseInt(odds[o].wins),
-      loses = parseInt(odds[o].loses),
-      total = wins + loses,
-      percentage = (wins/total) * 100;
+    var wins = parseInt(odds[o].wins),
+        loses = parseInt(odds[o].loses),
+        total = wins + loses,
+        percentage = (wins/total) * 100;
 
-  console.log( o + ': ' + Math.round(percentage) + '% - total ' + total);
-}
+    console.log( o + ': ' + Math.round(percentage) + '% - total ' + total);
+  }
 }
 
 console.log(odds);
 calcPercentages();
+
+
+var odds = {},
+    lines = document.getElementsByClassName('line-bet');
+
+for (var i = 0; i <= lines.length; i++) {
+  var thisLine = lines[i];
+  var event = thisLine.getElementsByClassName('event')[0].innerText.split(',')[0];
+
+  console.log(event);
+};
