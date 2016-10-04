15273491e160ed7b6549ef2b7f93a63c351ba3c9
Luis Rita <luismestrerita@gmail.com>
2016-10-04 09:05:07 +0100

    place executable code on separate file

--- popup.js
+++ popup.js
@@ -1,64 +1,44 @@
 var odds = {},
-    lines = document.getElementsByClassName('line-bet');
+    lines = document.getElementById("ended-bets").getElementsByClassName("line-bet");
 
-for (var i = 0; i <= lines.length; i++) {
-  var thisLine = lines[i];
-
-  console.log(lines[i]);
-  var event = $(thisLine).find('.event').html().split(',')[0],
-      odd = $(thisLine).find('.odd p').html(),
-      canceled = $(thisLine).find('.result').hasClass('nofinish'),
-      result = $(thisLine).find('.result').hasClass('win') && !canceled ? 'win' : 'lost',
+for (let i = 0; i < lines.length; i++) {
+  let thisLine = lines[i],
+      event = thisLine.getElementsByClassName("event")[0].innerText.split(",")[0],
+      odd = thisLine.getElementsByClassName("odd")[0].querySelector("p").innerText,
+      result = thisLine.getElementsByClassName("result")[0].className.split(" ")[1],
+      canceled = result === "nofinish" ? true : false,
       repeated = false;
-
-  if(event !== 'Futebol') {
-
+  if(event !== "Futebol" && !canceled) {
     // Get unique values that are on the odds
-    var objArray = Object.keys(odds);
+    let objArray = Object.keys(odds);
     objArray.forEach(function (item, index) {
       if(odd === item) {
-        if(result === 'win') {
-          odds[odd].wins += 1;
-        } else {
-          odds[odd].loses += 1;
-        }
-
+        odds[odd][result] += 1;
         repeated = true;
       }
     });
-
     // Push new unique value to odds
-    if(!repeated && !canceled) {
+    if(!repeated) {
       // odds.push(odd);
       odds[odd] = {
-        wins: result === 'win' ? 1 : 0,
-        loses: result === 'lose' ? 1 : 0,
+        win: result === "win" ? 1 : 0,
+        lose: result === "lose" ? 1 : 0,
+        calcPercentages: function() {
+          let wins = parseInt(this.win),
+              loses = parseInt(this.lose),
+              total = wins + loses,
+              percentage = (wins/total) * 100;
+          return odd + ": " + Math.round(percentage) + "% - total " + total;
+        }
       };
     }
   }
 };
 
-function calcPercentages() {
-  for(var o in odds) {
-    var wins = parseInt(odds[o].wins),
-        loses = parseInt(odds[o].loses),
-        total = wins + loses,
-        percentage = (wins/total) * 100;
+var keys = Object.keys(odds);
+keys.sort();
 
-    console.log( o + ': ' + Math.round(percentage) + '% - total ' + total);
-  }
+for (var k = 0; k < keys.length; k++) {
+  var o = keys[k];
+  console.log(odds[o].calcPercentages());
 }
-
-console.log(odds);
-calcPercentages();
-
-
-var odds = {},
-    lines = document.getElementsByClassName('line-bet');
-
-for (var i = 0; i <= lines.length; i++) {
-  var thisLine = lines[i];
-  var event = thisLine.getElementsByClassName('event')[0].innerText.split(',')[0];
-
-  console.log(event);
-};
--- popup_2.js
+++ popup_2.js
@@ -1,50 +1,5 @@
 function getStats() {
-  chrome.tabs.executeScript({
-    code: 'var odds = {},' +
-              'lines = document.getElementById("ended-bets").getElementsByClassName("line-bet");' +
-
-          'for (let i = 0; i < lines.length; i++) {' +
-            'let thisLine = lines[i],' +
-                'event = thisLine.getElementsByClassName("event")[0].innerText.split(",")[0],' +
-                'odd = thisLine.getElementsByClassName("odd")[0].querySelector("p").innerText,' +
-                'result = thisLine.getElementsByClassName("result")[0].className.split(" ")[1],' +
-                'canceled = result === "nofinish" ? true : false,' +
-                'repeated = false;' +
-            'if(event !== "Futebol" && !canceled) {' +
-              // Get unique values that are on the odds
-              'let objArray = Object.keys(odds);' +
-              'objArray.forEach(function (item, index) {' +
-                'if(odd === item) {' +
-                  'odds[odd][result] += 1;' +
-                  'repeated = true;' +
-                '}' +
-              '});' +
-              // Push new unique value to odds
-              'if(!repeated) {' +
-                // odds.push(odd);
-                'odds[odd] = {' +
-                  'win: result === "win" ? 1 : 0,' +
-                  'lose: result === "lose" ? 1 : 0,' +
-                  'calcPercentages: function() {' +
-                    'let wins = parseInt(this.win),' +
-                        'loses = parseInt(this.lose),' +
-                        'total = wins + loses,' +
-                        'percentage = (wins/total) * 100;' +
-                    'return odd + ": " + Math.round(percentage) + "% - total " + total;' +
-                  '}' +
-                '};' +
-              '}' +
-            '}' +
-          '};' +
-
-          'var keys = Object.keys(odds);' +
-          'keys.sort();' +
-
-          'for (var k = 0; k < keys.length; k++) {' +
-            'var o = keys[k];' +
-            'console.log(odds[o].calcPercentages());' +
-          '}'
-  });
+  chrome.tabs.executeScript(null, {file: 'popup.js'});
 };
 
 function init() {
