614f8fd4df2af6922fd9a708e0e36b6f5986d303
Luis Rita <luismestrerita@gmail.com>
2016-10-04 10:41:12 +0100

    add content script file that scrolls down automaticaly and gets the stats in the end

--- content.js
+++ content.js
@@ -1,9 +1,65 @@
-// Listen for messages
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    // If the received message has the expected format...
-    if (msg.text === 'report_back') {
-        // Call the specified callback, passing
-        // the web-page's DOM content as argument
-        sendResponse(document.all[0].outerHTML);
+function scrollDown() {
+  var loadMore;
+
+  autoScroll = setInterval(function() {
+    loadMore = document.getElementById('load-more');
+
+    if(loadMore !== null) {
+        window.scrollTo(0, document.body.scrollHeight);
+    } else {
+      clearInterval(autoScroll);
+      calcOds();
     }
-});
+  }, 1000); 
+}
+
+scrollDown();
+
+
+
+function calcOds() {
+  var odds = {},
+      lines = document.getElementById("ended-bets").getElementsByClassName("line-bet");
+
+  for (let i = 0; i < lines.length; i++) {
+    let thisLine = lines[i],
+        event = thisLine.getElementsByClassName("event")[0].innerText.split(",")[0],
+        odd = thisLine.getElementsByClassName("odd")[0].querySelector("p").innerText,
+        result = thisLine.getElementsByClassName("result")[0].className.split(" ")[1],
+        canceled = result === "nofinish" ? true : false,
+        repeated = false;
+    if(event !== "Futebol" && !canceled) {
+      // Get unique values that are on the odds
+      let objArray = Object.keys(odds);
+      objArray.forEach(function (item, index) {
+        if(odd === item) {
+          odds[odd][result] += 1;
+          repeated = true;
+        }
+      });
+      // Push new unique value to odds
+      if(!repeated) {
+        // odds.push(odd);
+        odds[odd] = {
+          win: result === "win" ? 1 : 0,
+          lose: result === "lose" ? 1 : 0,
+          calcPercentages: function() {
+            let wins = parseInt(this.win),
+                loses = parseInt(this.lose),
+                total = wins + loses,
+                percentage = (wins/total) * 100;
+            return odd + ": " + Math.round(percentage) + "% - total " + total;
+          }
+        };
+      }
+    }
+  };
+
+  var keys = Object.keys(odds);
+  keys.sort();
+
+  for (var k = 0; k < keys.length; k++) {
+    var o = keys[k];
+    console.log(odds[o].calcPercentages());
+  } 
+}
